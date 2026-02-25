8c86b6ff6eef689e5e415abfe416c391d4b4964a
RODATOURCL <58814728+rodatourcl@users.noreply.github.com>
2026-02-25 18:00:03 +0100

    Delete js directory

--- js/header.js
+++
@@ -1,6 +0,0 @@
-fetch('../header.html')
-.then(response => response.text())
-.then(data => {
-    document.getElementById('header-container').innerHTML = data;
-})
-.catch(error => console.error('Error al cargar el header:', error));
